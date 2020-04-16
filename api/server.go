d76f9281cd141ccc6387db429c4707e2b4bcdb60
Atul <kkmonlee@gmail.com>
2020-04-17 04:11:01 +0530

    main run node

---
+++ api/server.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/kkmonlee/gemini-backend/gemini-backend/api/controllers"
+	"github.com/kkmonlee/gemini-backend/gemini-backend/api/seed"
+)
+
+var server = controllers.Server{}
+
+func Run() {
+	var err error
+	const port := 9000
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error retrieving env: %v", err)
+	} else {
+		fmt.Println("Successfully retrieved env values")
+	}
+
+	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+
+	seed.Load(server.DB)
+
+	server.Run(":" + port)
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kkmonlee/gemini-backend/gemini-backend/api"
+)
+
+func main() {
+	api.Run()
+}
